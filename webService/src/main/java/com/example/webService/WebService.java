cb8bdceac6df816ff9f9cbd6ffdbe70b5cb8abdf
root <benoit.charroux@gmail.com>
2020-03-12 14:42:18 +0100

    code

--- webService/src/main/java/com/example/webService/WebService.java
+++ webService/src/main/java/com/example/webService/WebService.java
@@ -2,6 +2,8 @@ package com.example.webService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBoby;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,4 +16,10 @@ public class WebService {
     public Iterable<Car> getCars(){
         return carRepository.findAll();
     }
+
+    @PostMapping("/cars")
+    public void addCar(@RequestBoby Car car){
+        carRepository.save(car);
+    }
+	
 }
